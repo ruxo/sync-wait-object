48a4f7a9bf62c0afadacf9091d0d3b478bab1ec3
Ruxo Zheng <rz@just.sent.as>
2023-01-19 21:26:35 +0700

    Implement wait and reset.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
-use std::{
-    time, time::Duration,
-    ops::Deref,
-    sync::{Arc, Condvar, Mutex, MutexGuard}
-};
+use std::{time, time::Duration, ops::Deref, sync::{Arc, Condvar, Mutex, MutexGuard}, mem};
+use std::ops::DerefMut;
 
 // ------------------------------ DATA STRUCTURE ------------------------------
 #[derive(Debug)]
@@ -17,6 +14,8 @@ pub type Result<T> = std::result::Result<T, WaitObjectError>;
 ///
 /// This wait object is just a wrapper of Mutex and Condvar combination with the suggested pattern (from Rust document) for waiting a value.
 ///
+/// There are two ways to wait. The first is just to want until an expected value.
+///
 /// ```rust
 /// # use sync_wait_object::WaitEvent;
 /// use std::thread;
@@ -29,9 +28,33 @@ pub type Result<T> = std::result::Result<T, WaitObjectError>;
 ///     }
 /// });
 ///
-/// let r#final = *wait3.wait(|i| *i == 3, None).unwrap();
+/// let timeout = std::time::Duration::from_secs(1);
+/// let r#final = *wait3.wait(Some(timeout), |i| *i == 3).unwrap();
+/// let current = *wait3.value().unwrap();
 /// assert_eq!(r#final, 3);
+/// assert_eq!(current, 3);
 /// ```
+///
+/// The second is to wait and then reset the value to a desired state.
+/// ```rust
+/// # use sync_wait_object::WaitEvent;
+/// use std::thread;
+/// let wait3 = WaitEvent::new_init(0);
+/// let mut wait_handle = wait3.clone();
+///
+/// thread::spawn(move || {
+///     for i in 1..=3 {
+///         wait_handle.set_state(i).unwrap();
+///     }
+/// });
+///
+/// let timeout = std::time::Duration::from_secs(1);
+/// let r#final = wait3.wait_reset(Some(timeout), || 1, |i| *i == 3).unwrap();
+/// let current = *wait3.value().unwrap();
+/// assert_eq!(r#final, 3);
+/// assert_eq!(current, 1);
+/// ```
+///
 #[derive(Clone)]
 pub struct WaitEvent<T>(Arc<(Mutex<T>, Condvar)>);
 
@@ -48,11 +71,21 @@ impl<T> WaitEvent<T> {
         Self(Arc::new((Mutex::new(initial_state), Condvar::new())))
     }
 
-    #[inline]
-    pub fn wait(&self, checker: impl FnMut(&T) -> bool, timeout: Option<Duration>) -> Result<MutexGuard<T>> {
+    pub fn value(&self) -> Result<MutexGuard<T>> {
+        self.0.0.lock().map_err(|e| e.into())
+    }
+
+    pub fn wait(&self, timeout: Option<Duration>, checker: impl FnMut(&T) -> bool) -> Result<MutexGuard<T>> {
         match timeout {
             Some(t) => self.wait_with_waiter(checker, Self::waiter(t)),
             None => self.wait_with_waiter(checker, Self::no_waiter())
+        }
+    }
+
+    pub fn wait_reset(&self, timeout: Option<Duration>, reset: impl FnMut() -> T, checker: impl FnMut(&T) -> bool) -> Result<T> {
+        match timeout {
+            Some(t) => self.wait_and_reset_with_waiter(checker, Self::waiter(t), reset),
+            None => self.wait_and_reset_with_waiter(checker, Self::no_waiter(), reset)
         }
     }
 
@@ -63,6 +96,11 @@ impl<T> WaitEvent<T> {
             state = cond.wait(state)?;
         }
         Ok(state)
+    }
+
+    fn wait_and_reset_with_waiter(&self, checker: impl FnMut(&T) -> bool, waiter: impl Fn() -> bool, mut reset: impl FnMut() -> T) -> Result<T> {
+        let state = self.wait_with_waiter(checker, waiter);
+        state.map(|mut g| mem::replace(g.deref_mut(), reset()))
     }
 
     fn waiter(timeout: Duration) -> impl Fn() -> bool {
@@ -90,11 +128,11 @@ impl ManualResetEvent {
 
     #[inline]
     pub fn wait_until_set(&self) -> Result<bool> {
-        self.0.wait(|v| *v, None).map(|g| *g)
+        self.0.wait(None, |v| *v).map(|g| *g)
     }
 
     #[inline] pub fn wait_one(&self, timeout: Duration) -> Result<bool> {
-        self.0.wait(|v| *v, Some(timeout)).map(|g| *g)
+        self.0.wait(Some(timeout), |v| *v).map(|g| *g)
     }
 
     #[inline]
